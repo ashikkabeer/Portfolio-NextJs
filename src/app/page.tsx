910d49be4d21ee30e2cad696306af44083f34a6d
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-01-27 13:46:55 +0530

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import { SpotlightPreview } from "@/components/Spotlight";
+import { MeteorsDemo } from "@/components/Meteor";
+import { NavbarDemo } from "@/components/Navbar";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between md:align-middle">
+      
       <SpotlightPreview/>
+      <NavbarDemo/>
+      <MeteorsDemo/>
     </main>
   );
 }
