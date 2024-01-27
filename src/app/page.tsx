fb56ced70d95c6a86f67ea65a8918b0846fca32d
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-01-27 13:24:05 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { SpotlightPreview } from "@/components/Spotlight";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between md:align-middle">
       <SpotlightPreview/>
     </main>
   );
